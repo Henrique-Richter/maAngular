13c8fbc56e7a587886a075f51d8f3f7b4720440e
Henrique Richter <Henrique Richter>
2016-04-23 16:21:21 -0300

    Visualização do carrinho parcial e adicionar no carrinho pela pesquisa

--- app/indexController.js
+++ app/indexController.js
@@ -2,9 +2,51 @@
 
 angular.module('myApp.indexController', ['ngRoute'])
 
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/carrinho', {
+    templateUrl: 'carrinho/visualizarCarrinho.html',
+    controller: 'indexCtrl'
+  });
+}])
 
 .controller('indexCtrl', function($scope, $http,$timeout,$location) {
 
+
+
+  this.produto={
+    id: "" ,
+    quantidade: "",
+    titulo: "",
+    preco: ""
+  };
+
+  this.carrinho =[];
+
+    this.adicionarCarrinho= function(id,quantidade,titulo,preco){
+    //  console.log(id,quantidade,titulo,preco);
+
+      this.produto.id=id;
+      this.produto.quantidade=quantidade;
+      this.produto.titulo=titulo;
+      this.produto.preco=preco;
+      var copia=angular.copy(this.produto);
+
+      var found = false;
+      for(var i = 0; i < this.carrinho.length; i++) {
+          if (this.carrinho[i].id == copia.id) {
+              found = true;
+              break;
+          }
+      }
+
+      if(found==false){
+        this.carrinho.push(copia);
+       alert("Adicionado no carrinho");
+     }else{
+       alert("Já existe no carrinho");
+     }
+
+    };
 
   //$scope.counter = 100;
 
@@ -23,10 +65,15 @@ angular.module('myApp.indexController', ['ngRoute'])
     autor:null
   }
 
-  $scope.countdown = function() {
-    var timerCount = 1200;
 
-    var countDown = function () {
+
+
+
+
+    $scope.countdown = function() {
+      var   timerCount = 1200;
+
+      var countDown = function () {
       if (timerCount < 0) {
         //Any desired function upon countdown end.
         //$window.close();
@@ -48,6 +95,7 @@ angular.module('myApp.indexController', ['ngRoute'])
     countDown();
 
   };
+
   this.resetTime = function(){
     console.log("teste ok");
     timerCount= 1200;
