b7bd000043f6127952a854f287e263c78f499302
Lucas Chiaparini <chiaparini@Lucass-MacBook-Pro.local>
2016-04-30 16:51:58 -0300

    Feito reinicializacao do tempo do carrinho quando muda de estado

--- app/indexController.js
+++ app/indexController.js
@@ -3,137 +3,139 @@
 angular.module('myApp.indexController', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/carrinho', {
-    templateUrl: 'carrinho/visualizarCarrinho.html',
-    controller: 'indexCtrl'
-  });
+	$routeProvider.when('/carrinho', {
+		templateUrl: 'carrinho/visualizarCarrinho.html',
+		controller: 'indexCtrl'
+	});
 }])
 
-.controller('indexCtrl', function($scope, $http,$timeout,$location) {
+.controller('indexCtrl', function($scope, $http,$timeout,$location,  $rootScope) {
 
 
-  this.removerDoCarrinho=function($index){
-    this.carrinho.splice($index,1);
-  }
-  this.atualizarCarrinho=function(value,$index){
+	this.removerDoCarrinho=function($index){
+		this.carrinho.splice($index,1);
+	}
+	this.atualizarCarrinho=function(value,$index){
 
-    var copia=angular.copy(this.carrinho[$index]);
+		var copia=angular.copy(this.carrinho[$index]);
 
-    copia.quantidade=copia.quantidade + value;
-    this.carrinho.splice($index, 1);
-    this.carrinho.splice($index, 0, copia);
-  }
+		copia.quantidade=copia.quantidade + value;
+		this.carrinho.splice($index, 1);
+		this.carrinho.splice($index, 0, copia);
+	}
 
-  this.produto={
-    id: null ,
-    quantidade: null,
-    titulo: null,
-    preco: null,
-    estoque: null
-  };
+	$scope.$on('$routeChangeStart', function(next, current) { 
+		resetTime();
+	});
 
-  this.carrinho =[];
+	this.produto={
+		id: null ,
+		quantidade: null,
+		titulo: null,
+		preco: null,
+		estoque: null
+	};
 
-    this.adicionarCarrinho= function(id,quantidade,titulo,preco,estoque){
-     console.log(id,quantidade,titulo,preco,estoque);
+	this.carrinho =[];
 
-      this.produto.id=id;
-      this.produto.quantidade=quantidade;
-      this.produto.titulo=titulo;
-      this.produto.preco=preco;
-      this.produto.estoque = estoque;
+	this.adicionarCarrinho= function(id,quantidade,titulo,preco,estoque){
+		console.log(id,quantidade,titulo,preco,estoque);
 
-      var copia=angular.copy(this.produto);
+		this.produto.id=id;
+		this.produto.quantidade=quantidade;
+		this.produto.titulo=titulo;
+		this.produto.preco=preco;
+		this.produto.estoque = estoque;
 
-      var found = false;
-      for(var i = 0; i < this.carrinho.length; i++) {
-          if (this.carrinho[i].id == copia.id) {
-              found = true;
-              break;
-          }
-      }
+		var copia=angular.copy(this.produto);
 
-      if(found==false){
-        this.carrinho.push(copia);
-       alert("Adicionado no carrinho");
-     }else{
-       alert("Já existe no carrinho");
-     }
+		var found = false;
+		for(var i = 0; i < this.carrinho.length; i++) {
+			if (this.carrinho[i].id == copia.id) {
+				found = true;
+				break;
+			}
+		}
 
-    };
+		if(found==false){
+			this.carrinho.push(copia);
+			alert("Adicionado no carrinho");
+		}else{
+			alert("Já existe no carrinho");
+		}
+
+	};
 
   //$scope.counter = 100;
 
   this.resetar = function(){
-    this.pesquisa={
-      titulo:null,
-      editora:null,
-      categoria:null,
-      autor:null
-    }
+  	this.pesquisa={
+  		titulo:null,
+  		editora:null,
+  		categoria:null,
+  		autor:null
+  	}
   };
   this.pesquisa={
-    titulo:null,
-    editora:null,
-    categoria:null,
-    autor:null
+  	titulo:null,
+  	editora:null,
+  	categoria:null,
+  	autor:null
   }
 
 
-//metodo para pegar o total
+	//metodo para pegar o total
 
-  this.getTotal = function(){
-    var total = 0;
-    for(var i = 0; i < this.carrinho.length; i++){
-        var product = this.carrinho[i];
-        total += (product.preco * product.quantidade);
-    }
-    return total;
-}
+	this.getTotal = function(){
+		var total = 0;
+		for(var i = 0; i < this.carrinho.length; i++){
+			var product = this.carrinho[i];
+			total += (product.preco * product.quantidade);
+		}
+		return total;
+	}
+	var  timerCount = 1200;
+	$scope.countdown = function() {
 
+		var countDown = function () {
+			if (timerCount < 0) {
+	        //Any desired function upon countdown end.
+	        //  this.carrinho=[];
+	        $location.path("index");
+	        console.log("Terminou");
+	    } else {
+	    	var minutes = Math.floor(timerCount / 60);
+	    	var seconds = timerCount - minutes * 60;
 
-    $scope.countdown = function() {
-      var   timerCount = 1200;
+	    	$scope.counter = " " + minutes + " : "+ seconds;
+	    	timerCount--;
+	    	$timeout(countDown, 1000);
+	    }
+	};
+	var minutes = Math.floor(timerCount / 60);
+	var seconds = timerCount - minutes * 60;
 
-      var countDown = function () {
-      if (timerCount < 0) {
-        //Any desired function upon countdown end.
-        //  this.carrinho=[];
-          $location.path("index");
-        console.log("Terminou");
-      } else {
-        var minutes = Math.floor(timerCount / 60);
-        var seconds = timerCount - minutes * 60;
+	$scope.counter = " " + minutes + " : "+ seconds;
 
-        $scope.counter = " " + minutes + " : "+ seconds;
-        timerCount--;
-        $timeout(countDown, 1000);
-      }
-    };
-    var minutes = Math.floor(timerCount / 60);
-    var seconds = timerCount - minutes * 60;
+	countDown();
 
-    $scope.counter = " " + minutes + " : "+ seconds;
-
-    countDown();
-
-  };
+	};
 
 
-  this.resetTime = function(){
-    console.log("teste ok");
-    timerCount= 1200;
-  };
+	var resetTime = function(){
+		console.log("teste ok");
+		timerCount= 1200;
+	};
 
-  this.pesquisar= function(){
-    console.log(this.pesquisa);
-    $location.path("listarLivro");
-    $http.post('http://localhost:8000/api/livro/busca',this.pesquisa).success(function(data, status, headers, config){
-      $scope.livros= data;
-      console.log(data, status, headers, config);
-    });
-  //  console.log("teste redirecionar");
-    this.resetar();
-  };
+	this.pesquisar= function(){
+		console.log(this.pesquisa);
+		$location.path("listarLivro");
+		$http.post('http://localhost:8000/api/livro/busca',this.pesquisa).success(function(data, status, headers, config){
+			$scope.livros= data;
+			console.log(data, status, headers, config);
+		});
+	  //  console.log("teste redirecionar");
+	  this.resetar();
+};
 
 });
